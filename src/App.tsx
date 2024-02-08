96301d65fe6ee17b6d581e956a9481a6bf336bfb
Griffoo91 <104443656+Griffoo91@users.noreply.github.com>
2024-02-08 11:54:50 +0300

    second commit:setting up routes && installing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import './globals.css';
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-  <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
-}
+  return(
+    <main className="flex h-screen">
+      <Routes>
+        { /*public routes */}
 
-export default App
+        <Route element={ <AuthLayout/>} >
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        
+        {/*private routes */}
+        
+        <Route element= {<RootLayout/>}>
+          <Route index element={<Home />} />
+        </Route>
+        
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDom from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDom.createRoot(document.getElementById("root")!).render(
-<App />
-);
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
