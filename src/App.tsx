707f57c24b6a36c158b1c9d68e7d7310682e1947
Griffoo91 <104443656+Griffoo91@users.noreply.github.com>
2024-02-09 11:49:10 +0300

    fourth commit:inclusion of auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />t
         </Route>
 
         {/*private routes */}
